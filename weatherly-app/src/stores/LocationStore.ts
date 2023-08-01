d389db0a98b47b740b82acef858cb36389df7448
Anatoliy Agadzhanov <aanatoliy17@gmail.com>
2023-08-01 04:22:50 -0400

    created state in Location store using composition api

--- weatherly-app/src/stores/LocationStore.ts
+++ weatherly-app/src/stores/LocationStore.ts
@@ -1,7 +1,9 @@
 import { defineStore } from 'pinia'
 // import axios from 'axios'
+import { ref } from 'vue'
 
-export const useLocationStore = defineStore('LocationStore', {
-    
-    
+export const useLocationStore = defineStore('LocationStore', () => {
+    const currentWeather = ref(null)
+    const dailyWeather = ref([])
+    return { currentWeather, dailyWeather }
 })
