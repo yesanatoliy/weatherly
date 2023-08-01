47c0639dc4b2936c9bb84f5bf0fbf2b5da1cad62
Anatoliy Agadzhanov <aanatoliy17@gmail.com>
2023-08-01 05:51:33 -0400

    accessed location store in the app component, want to test out the api call

--- weatherly-app/src/App.vue
+++ weatherly-app/src/App.vue
@@ -7,6 +7,8 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HomePage from './pages/HomePage.vue'
+import { useLocationStore } from "@/stores/LocationStore"
+const locationStore = useLocationStore()
 export default defineComponent({
   components: {
     HomePage
