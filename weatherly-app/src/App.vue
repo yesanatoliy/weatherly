3e78f29019f358d58e0e392381b6a0f5b69ca7fb
Anatoliy Agadzhanov <aanatoliy17@gmail.com>
2023-07-31 21:26:36 -0400

    finished initial setup with vue cli presets and making a css file

--- weatherly-app/src/App.vue
+++ weatherly-app/src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    hello
+
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { defineComponent } from 'vue';
 
-export default {
+export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
   }
-}
+});
 </script>
 
 <style>
