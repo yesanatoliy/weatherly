82cd3480a723b4c837a6122f4ea58b2ee3c4e71f
Anatoliy Agadzhanov <aanatoliy17@gmail.com>
2023-08-01 03:47:55 -0400

    edited Home component name to HomePage so that it renders, imported HomePage into the App component

--- weatherly-app/src/App.vue
+++ weatherly-app/src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="app">
+    <HomePage />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-
+import HomePage from './pages/Home.vue'
 export default defineComponent({
   name: 'App',
   components: {
+    HomePage
   },
   mounted: () => {
     // navigator.geolocation.getCurrentPosition
