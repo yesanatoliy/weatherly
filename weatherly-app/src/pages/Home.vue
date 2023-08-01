96caa4a29b7465dc0cc47e898c03fee87c6c3029
Anatoliy Agadzhanov <aanatoliy17@gmail.com>
2023-08-01 03:43:22 -0400

    created a pages directory and created boiler plate for Home page component

---
+++ weatherly-app/src/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>hi</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Home'
+  })
+</script>
+
+<style>
+
+</style>
