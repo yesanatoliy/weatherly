49ff211a11b9bcdfec6c0164b93619aa67338026
Anatoliy Agadzhanov <aanatoliy17@gmail.com>
2023-08-01 20:01:11 -0400

    added the script to the forecast component so that when that component mounts it invokes the api call to retrieve forecast data

--- weatherly-app/src/components/ForecastComponent.vue
+++ weatherly-app/src/components/ForecastComponent.vue
@@ -5,7 +5,13 @@
 </template>
 
 <script setup lang="ts">
-
+    import { useLocationStore } from '../stores/LocationStore';
+    import { onMounted } from 'vue'
+    const locationStore = useLocationStore();
+    onMounted(() =>
+        navigator.geolocation.getCurrentPosition(async (position) => {
+            await locationStore.getDailyWeather(position.coords)
+        }))
 </script>
 
 <style>
