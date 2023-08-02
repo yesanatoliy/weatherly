48a617c05388a5bde67d2f0f6e2673c6cd1ca413
Anatoliy Agadzhanov <aanatoliy17@gmail.com>
2023-08-02 01:19:22 -0400

    installed vuetify and added it to the main.ts file... looks good but i'm nervous the app is going to break and now i have to refactor my components

--- weatherly-app/src/main.ts
+++ weatherly-app/src/main.ts
@@ -2,9 +2,19 @@ import './assets/global.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 const pinia = createPinia()
 const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
-app.use(pinia)
-app.mount('#app')
+app.use(pinia).use(vuetify).mount('#app')
+
+
+
